c69ca4e3507a9136ace4cea5bba830445552ee31
Ivan Aslamov <ivan.aslamov@gmail.com>
2016-05-01 08:32:49 -0700

    improvement

--- build/README.md
+++
@@ -1,7 +0,0 @@
-Folder to build project.
-
-To build:
-1. cd to this folder
-2. cmake ..
-3. make
-4. ./main
--- test/test.cpp
+++ test/test.cpp
@@ -3,7 +3,6 @@
 
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
-#include <opencv2/core/utility.hpp>
 
 #include "detect.h"
 
