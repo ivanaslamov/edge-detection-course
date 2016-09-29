dff5343f8e36b3fc58850bb409fc0def442a28be
Ivan Aslamov <ivan.aslamov@gmail.com>
2016-09-29 01:04:07 -0700

    improvement added flip functionality

--- src/cpp/utils.cpp
+++ src/cpp/utils.cpp
@@ -5,5 +5,15 @@ using namespace std;
 
 cv::Mat cv_flip(cv::Mat src)
 {
-	return src;
+	cv::Mat dst = cv::Mat(src.rows, src.cols, CV_8UC3);
+
+	for(int y=0; y<dst.rows; y++) {
+		for(int x=0; x<dst.cols; x++) {
+			for(int c=0; c<dst.channels(); c++) {
+				dst.data[(dst.rows - y - 1)*dst.step + x*dst.channels() + c] = src.data[y*dst.step + x*dst.channels() + c];
+			}
+		}
+	}
+
+	return dst;
 }
--- test/test.cpp
+++ test/test.cpp
@@ -9,7 +9,34 @@
 
 BOOST_AUTO_TEST_CASE(FlipTest)
 {
-	cv::Mat src = cv::Mat::zeros(100, 100, CV_8UC1);
+	cv::Mat src = cv::Mat::ones(100, 100, CV_8UC3);
 	cv::Mat result = cv_flip(src);
-    BOOST_CHECK(cv::sum(src) == cv::sum(result));
+	cv::Scalar src_sum = cv::sum(src);
+	cv::Scalar result_sum = cv::sum(result);
+	BOOST_CHECK(src_sum == result_sum);
+}
+
+
+BOOST_AUTO_TEST_CASE(FlipTestAll)
+{
+	cv::Mat src = cv::Mat::zeros(3, 1, CV_8UC3);
+	src.data[0] = 1;
+	src.data[1] = 1;
+	src.data[2] = 1;
+	src.data[3] = 2;
+	src.data[4] = 2;
+	src.data[5] = 2;
+	src.data[6] = 3;
+	src.data[7] = 3;
+	src.data[8] = 3;
+	cv::Mat dst = cv_flip(src);
+    BOOST_CHECK(dst.data[0] == 3);
+    BOOST_CHECK(dst.data[1] == 3);
+    BOOST_CHECK(dst.data[2] == 3);
+    BOOST_CHECK(dst.data[3] == 2);
+    BOOST_CHECK(dst.data[4] == 2);
+    BOOST_CHECK(dst.data[5] == 2);
+    BOOST_CHECK(dst.data[6] == 1);
+    BOOST_CHECK(dst.data[7] == 1);
+    BOOST_CHECK(dst.data[8] == 1);
 }
